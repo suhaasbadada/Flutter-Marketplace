e55ad850d6b63a7b1896c544a1c75ef9cb7eb165
suhaasbadada <suhaasbadada@gmail.com>
2022-09-16 10:51:46 +0530

    ninth commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,14 +37,45 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)?ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
-          },
-        ):const Center(child:CircularProgressIndicator()),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration:
+                            const BoxDecoration(color: Colors.deepPurple),
+                        child: Text(
+                          item.name,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      
+                      footer: Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: const BoxDecoration(
+                            color: Colors.black,
+                          ),
+                          child: Text("\$${item.price}",
+                              style: const TextStyle(color: Colors.white))),
+                      
+                      child: Image.network(item.imgURL),
+                    ),
+                  );
+                },
+              )
+            : const Center(child: CircularProgressIndicator()),
       ),
       drawer: const MyDrawer(),
     );
